c989074115d0003b689d83072e3e613858d1e67a
codetill5 <mehrashubham216@gmail.com>
2023-04-20 23:35:26 +0530

    optimized model with all methods

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ const gltfPipeline = require("gltf-pipeline");
 const fsExtra = require("fs-extra");
 const ngrok = require("ngrok");
 const request = require("request");
-const { simplify, weld, inspect } = require("@gltf-transform/functions");
+const sharp = require('sharp');
+const {
+  simplify,
+  weld,
+  inspect,
+  quantize,
+  instance,
+  dequantize,
+  resample,
+  prune,
+  draco,
+  join,
+  flatten,
+  sparse,
+  textureCompress,
+  dedup,
+} = require("@gltf-transform/functions");
 const { MeshoptSimplifier } = require("meshoptimizer");
+
 const { NodeIO } = require("@gltf-transform/core");
 const obj2gltf = require("obj2gltf");
+const draco3d = require("draco3dgltf");
 
 const io = new NodeIO();
 
@@ -15,28 +33,54 @@ const app = express();
 app.post("/compression", async (req, res) => {
   const processGlb = gltfPipeline.processGlb;
 
-  const glb = fsExtra.readFileSync("smooth.glb");
-  const options = {
+  const glb = fsExtra.readFileSync("gas.glb");
+const options = {
     dracoOptions: {
       compressionLevel: 10,
+      // compressMeshes: true,
+      // quantizePositionBits: 6
     },
   };
 
   processGlb(glb, options).then(function (results) {
-    fsExtra.writeFileSync("model-drac.glb", results.glb);
+    fsExtra.writeFileSync("gasDraco.glb", results.glb);
   });
 });
 
 app.post("/simplify", async (req, res) => {
   let document;
-  document = await io.read("smooth.glb");
+  document = await io.read("christmas2.glb");
 
   await document.transform(
     weld({ tolerance: 0.0001 }),
     simplify({ simplifier: MeshoptSimplifier, ratio: 0.75, error: 0.001 })
   );
 
-  await io.write("modelSimple.glb", document);
+  await io.write("simplechrist.glb", document);
+});
+
+//Optimize model by all available methods 
+app.post("/optimize", async (req, res) => {
+  let document;
+  document = await io.read("washinton.glb");
+
+  await document.transform(
+    // dedup(),
+    // instance({min: 2}),
+    // flatten(),
+    // dequantize(),
+    // join(),
+    weld({ tolerance: 0.0001 }),     //WORKS
+    simplify({ simplifier: MeshoptSimplifier, ratio: 0.75, error: 0.001 }),   //WORKS
+    // resample(),
+    // prune(),
+    // sparse(),
+    textureCompress({encoder: sharp,  targetFormat: 'webp'}),    
+    draco()   //WORKS
+  );
+
+  
+  await io.write("dc.glb", document);
 });
 
 app.get("/get-properties", async (req, res) => {
@@ -44,19 +88,19 @@ app.get("/get-properties", async (req, res) => {
   document = await io.read("smooth.glb");
 
   const response = await inspect(document);
-  const result = response.meshes
+  const result = response.meshes;
 
   let totalVertices = 0;
   let totalTriangles = 0;
 
-  result.properties.forEach(obj => {
-      totalVertices += obj.vertices;
-      totalTriangles += obj.glPrimitives
+  result.properties.forEach((obj) => {
+    totalVertices += obj.vertices;
+    totalTriangles += obj.glPrimitives;
   });
 
   res.send({
     totalVertices,
-    totalTriangles
+    totalTriangles,
   });
 });
 
@@ -70,24 +114,20 @@ app.post("/convert/glb-to-gltf", async (req, res) => {
 
 app.post("/convert/gltf-to-glb", async (req, res) => {
   const glbtoGLTF = gltfPipeline.gltfToGlb;
-  const gltf = fsExtra.readJsonSync("scene.gltf");
+  const gltf = fsExtra.readJsonSync("christmas.gltf");
   glbtoGLTF(gltf).then(function (results) {
-    fsExtra.writeFileSync("convert.glb", results.glb);
+    fsExtra.writeFileSync("christmas2.glb", results.glb);
   });
 });
-
 
 app.post("/convert/obj-to-glb", async (req, res) => {
   const options = {
     binary: true,
   };
-  obj2gltf("core/forage cap.obj", options).then(function (glb) {
-    fsExtra.writeFileSync("coreitbro.glb", glb);
+  obj2gltf("", options).then(function (glb) {
+    fsExtra.writeFileSync("googledrive.glb", glb);
   });
 });
-
-
-
 
 
 const port = 3002;
